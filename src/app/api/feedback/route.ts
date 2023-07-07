6711eb1083a561fa15493414014960ec9cbfb206
Joel <michaelbluth231@gmail.com>
2023-07-07 07:11:01 +0530

    03commit

---
+++ src/app/api/feedback/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+type Feedback = {
+  name?: string;
+  email?: string;
+  message?: string;
+};
+
+export async function POST(request: Request) {
+  const data: Feedback = await request.json();
+
+  console.log(data);
+
+  return NextResponse.json({
+    name: data.name,
+    email: data.email,
+    message: data.message,
+  });
+}
